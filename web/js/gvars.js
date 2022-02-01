acb5dd7c7ea8aedfe4b45afaf5a776b384a482c9
yuhengwu <799810767@qq.com>
2022-02-01 18:55:59 +0800

    Program refactor

--- web/js/gvars.js
+++ web/js/gvars.js
@@ -1,9 +1,34 @@
-var c_width = 1080
-var c_height = 608
-var BABYBLUE = '#92d7ef'
-var c_header_width = c_width / 2
-var c_header_height = c_height / 3
-var c_instr_width = c_width / 3
-var c_instr_height = c_instr_width / 8
-var c_cat_width = c_width / 18
-var c_cat_height = c_cat_width
+// window size
+const c_width = 1080
+const c_height = 608
+
+// frequently used colors
+const WHITE = '#ffffff'
+const RED = '#ff0000'
+const GREEN = '#00ff00'
+const BLUE = '#0000ff'
+const BLACK = '#000000'
+const GRAY = '#808080'
+const LIGHTGRAY = '#c0c0c0'
+const BABYBLUE = '#92d7ef'
+
+// main page
+const c_header_width = c_width / 2
+const c_header_height = c_height / 3
+const c_instr_width = c_width / 3
+const c_instr_height = c_instr_width / 8
+
+// cat
+const c_cat_width = c_width / 18
+const c_cat_height = c_cat_width
+
+// Red line coordinate, dimensions, and color
+const c_redline_left = c_width / 6
+const c_redline_top = 0
+const c_redline_width = 5
+const c_redline_height = c_height
+const c_redline_color = BLACK
+
+// player
+const c_player_width = c_width / 15
+const c_player_height = c_player_width
--- web/js/main.js
+++ web/js/main.js
@@ -1,46 +1,97 @@
-var config = {
+class StartScreen extends Phaser.Scene {
+    constructor() {
+        super({key: 'StartScreen'});
+    }
+
+    cursor = null;
+
+    preload() {
+        this.load.image('st-header', 'assets/header.png');
+        this.load.image('st-p1', 'assets/player_front1.png');
+        this.load.image('st-p2', 'assets/player_front2.png');
+        this.load.image('st-p3', 'assets/player_front3.png');
+        this.load.image('st-ins', 'assets/start_instr.png');
+        this.load.image('st-s1', 'assets/student_front1.png');
+        this.load.image('st-s2', 'assets/student_front2.png');
+        this.load.image('cat', 'assets/cat.png');
+    }
+
+    create() {
+        this.add.image(270, 152, 'st-header')
+            .setDisplaySize(c_header_width, c_header_height)
+            .setOrigin(0,0);
+        this.add.image(350, 380, 'st-ins')
+            .setDisplaySize(c_instr_width, c_instr_height)
+            .setOrigin(0,0);
+        this.add.image(150, 450, 'st-s1')
+            .setOrigin(0,0);
+        this.add.image(360, 450, 'st-p1')
+            .setOrigin(0,0);
+        this.add.image(520, 450, 'st-p2')
+            .setOrigin(0,0);
+        this.add.image(680, 450, 'st-p3')
+            .setOrigin(0,0);
+        this.add.image(840, 450, 'st-s2')
+            .setOrigin(0,0);
+        this.add.image(850, 370, 'cat')
+            .setDisplaySize(c_cat_width*2, c_cat_height*2)
+            .setOrigin(0,0);
+        
+        this.cursor = this.input.keyboard.createCursorKeys();
+    }
+
+    update() {}
+};
+
+class GameBody extends Phaser.Scene {
+    constructor() {
+        super({key: 'GameBody'});
+    }
+
+    cursor = null;
+
+    init() {
+        this.gameOver = false;
+        this.x = c_redline_left - 10;
+        this.y = c_height / 2;
+    }
+
+    preload() {
+        this.load.image('player', 'assets/player.png');
+        this.load.image('background', 'assets/background.png');
+        this.load.image('cat', 'assets/cat.png');
+        this.load.image('st-good', 'assets/student_good1.png');
+        this.load.image('st-bad', 'assets/student_bad1.png');
+        this.load.image('magic', 'assets/magic.png');
+    }
+
+    create() {
+        this.init();
+        this.cursor = this.input.keyboard.createCursorKeys();
+
+        this.player = this.physics.add.sprite(this.x, this.y, 'player').setInteractive();
+        this.player.setCollideWorldBounds(true);
+        this.player.body.setGravityY(0);
+
+        this.add.image(0,0,'background')
+            .setDisplaySize(c_width, c_height)
+            .setOrigin(0,0);
+    }
+};
+
+const config = {
     type: Phaser.AUTO,
     width: c_width,
     height: c_height,
     backgroundColor: BABYBLUE,
-    scene: {
-        preload: preload,
-        create: create
-    }
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: {y: 0},
+            debug: false
+        }
+    },
+    scene: [StartScreen]
 };
 
-var game = new Phaser.Game(config);
-
-function preload() {
-    // resources for start up page
-    this.load.image('st-header', 'assets/header.png');
-    this.load.image('st-p1', 'assets/player_front1.png');
-    this.load.image('st-p2', 'assets/player_front2.png');
-    this.load.image('st-p3', 'assets/player_front3.png');
-    this.load.image('st-ins', 'assets/start_instr.png');
-    this.load.image('st-s1', 'assets/student_front1.png');
-    this.load.image('st-s2', 'assets/student_front2.png');
-    this.load.image('cat', 'assets/cat.png');
-}
-
-function create() {
-    this.add.image(270, 152, 'st-header')
-        .setDisplaySize(c_header_width, c_header_height)
-        .setOrigin(0,0);
-    this.add.image(350, 380, 'st-ins')
-        .setDisplaySize(c_instr_width, c_instr_height)
-        .setOrigin(0,0);
-    this.add.image(150, 450, 'st-s1')
-        .setOrigin(0,0);
-    this.add.image(360, 450, 'st-p1')
-        .setOrigin(0,0);
-    this.add.image(520, 450, 'st-p2')
-        .setOrigin(0,0);
-    this.add.image(680, 450, 'st-p3')
-        .setOrigin(0,0);
-    this.add.image(840, 450, 'st-s2')
-        .setOrigin(0,0);
-    this.add.image(850, 370, 'cat')
-        .setDisplaySize(c_cat_width*2, c_cat_height*2)
-        .setOrigin(0,0);
-}
+const game = new Phaser.Game(config);
